498fe671f9c89311a7c39eeeccdd0345c57407d4
renaud gaudin <rgaudin@gmail.com>
2014-06-13 15:28:19 +0000

    added shared xls functions

--- snisi_core/xls_export.py
+++ snisi_core/xls_export.py
@@ -9,3 +9,22 @@ import StringIO
 
 def default_xls_export(report):
     return StringIO.StringIO()
+
+
+def xls_get_cell_repr(outSheet, colIndex, rowIndex):
+    """ HACK: Extract the internal xlwt cell representation. """
+    row = outSheet._Worksheet__rows.get(rowIndex)
+    if not row:
+        return None
+    cell = row._Row__cells.get(colIndex)
+    return cell
+
+
+def xls_update_value_only(outSheet, col, row, value):
+    """ Change cell value without changing formatting. """
+    previousCell = xls_get_cell_repr(outSheet, col, row)
+    outSheet.write(row, col, value)
+    if previousCell:
+        newCell = xls_get_cell_repr(outSheet, col, row)
+        if newCell:
+            newCell.xf_idx = previousCell.xf_idx
