6d69bf6ed1310aedbf4e5938cbf0f0957919b07d
renaud gaudin <rgaudin@gmail.com>
2014-08-18 15:25:04 +0000

    restored online url

--- snisi_web/static/src/js/malaria_mapping.js
+++ snisi_web/static/src/js/malaria_mapping.js
@@ -113,11 +113,9 @@ function getMalariaMapManager(options) {
         this.color_yes = '#889f37'; //'#28ff00';
         this.color_no = '#4d2c74'; //'#ff1500';
         this.colors = options.colors || ["#fef0d9", "#fdcc8a", "#fc8d59", "#d7301f"];
-        this.tiles_url_tmpl = 'http://tiles/#SLUG#-#SUFFIX#/{z}/{x}/{y}.png';
-        // this.base_layer_url = 'http://tiles.sante.gov.ml/mali-base/{z}/{x}/{y}.png';
-        this.base_layer_url = 'http://tiles/mali-base/{z}/{x}/{y}.png';
-        // this.base_layer_url = '/tiles/mali-base/{z}/{x}/{y}.png';
-        this.marker_url_tmpl = 'http://tiles/markers/#FILE#';
+        this.tiles_url_tmpl = 'http://tiles.sante.gov.ml/#SLUG#-#SUFFIX#/{z}/{x}/{y}.png';
+        this.base_layer_url = 'http://tiles.sante.gov.ml/mali-base/{z}/{x}/{y}.png';
+        this.marker_url_tmpl = 'http://tiles.sante.gov.ml/markers/#FILE#';
         this.initial_latitude = 14.0512;
         this.initial_longitude = -5.519499;
         this.initial_zoom = 8;
