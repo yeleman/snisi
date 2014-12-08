d075344dc99991432105cbbdc2e0fe5f42cea044
renaud gaudin <rgaudin@gmail.com>
2014-12-08 10:37:49 +0000

    added update provider

--- snisi_maint/management/commands/update-providers-from-std-csv.py
+++ snisi_maint/management/commands/update-providers-from-std-csv.py
@@ -15,6 +15,8 @@ from snisi_core.models.Providers import Provider
 from snisi_core.models.Roles import Role
 from snisi_tools.auth import create_provider
 from snisi_core.models.Entities import Entity
+from snisi_core.models.Numbers import PhoneNumber, PhoneNumberType
+from snisi_tools.numbers import normalized_phonenumber
 
 if PY2:
     import unicodecsv as csv
@@ -22,6 +24,15 @@ else:
     import csv
 
 logger = logging.getLogger(__name__)
+gender_map = {"M": Provider.MALE,
+              "F": Provider.FEMALE}
+title_map = {
+    "Mr": Provider.MISTER,
+    "Mme": Provider.MISS,
+    "Mlle": Provider.MISTRESS,
+    "Dr": Provider.DOCTOR,
+    "Pr": Provider.PROFESSOR
+}
 
 
 class Command(BaseCommand):
@@ -60,9 +71,6 @@ class Command(BaseCommand):
 
             if entry.get('action') == 'create':
                 logger.info("Creating {}".format(entry.get('last_name')))
-
-                gender_map = {"M": Provider.MALE,
-                              "F": Provider.FEMALE}
 
                 first_name = entry.get('first_name') or None
                 last_name = entry.get('last_name') or None
@@ -118,5 +126,55 @@ class Command(BaseCommand):
                 p.location = Entity.get_or_none(entry.get('location'))
                 p.save()
                 logger.info("MOVED,{}".format(p))
+            elif entry.get('action') == 'update':
+                p = Provider.get_or_none(entry.get('username'))
+                numbers = []
+                flotte = entry.get('flotte') or None
+                orange = entry.get('orange') or None
+                malitel = entry.get('malitel') or None
+                if flotte:
+                    numbers.append((flotte, True))
+                if orange:
+                    numbers.append((orange, False))
+                if malitel:
+                    numbers.append((malitel, False))
+
+                data = {
+                    'first_name': entry.get('first_name') or None,
+                    'last_name': entry.get('last_name') or None,
+                    'role': Role.get_or_none(entry.get('role')) or None,
+                    'location':
+                        Entity.get_or_none(entry.get('location')) or None,
+                    'email': entry.get('email') or None,
+                    'maiden_name': entry.get('maiden_name') or None,
+                    'middle_name': entry.get('other_names') or None,
+                    'gender':
+                        gender_map.get(entry.get('gender').upper()) or None,
+                    'title': title_map.get(entry.get('title')) or None,
+                    'position': entry.get('position') or None
+                }
+
+                update = False
+                for k, v in data.items():
+                    if v is not None:
+                        update = True
+                        setattr(p, k, v)
+                if update:
+                    p.save()
+
+                for number, is_flotte in numbers:
+                    npn = normalized_phonenumber(number)
+                    pnt = PhoneNumberType.from_number(npn, is_flotte=is_flotte)
+                    pn = PhoneNumber.get_or_none(npn)
+                    if pn is not None:
+                        pn.provider = p
+                    else:
+                        PhoneNumber.objects.create(
+                            identity=npn,
+                            category=pnt,
+                            priority=pnt.priority,
+                            provider=p)
+
+                logger.info("UPDATED,{}".format(p))
 
         logger.info("Done")
