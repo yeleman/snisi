466422a71fdab1310ce5533d58f4aa621c761b78
renaud gaudin <rgaudin@gmail.com>
2015-01-16 11:30:02 +0000

    new script to fix double expected in January

--- snisi_maint/management/commands/fix_double_expected.py
+++ snisi_maint/management/commands/fix_double_expected.py
@@ -7,11 +7,9 @@ from __future__ import (unicode_literals, absolute_import,
 import logging
 
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 
-from snisi_core.models.Projects import Cluster
-from snisi_core.models.Periods import MonthPeriod
 from snisi_core.models.Reporting import ExpectedReporting
-
 
 logger = logging.getLogger(__name__)
 
@@ -20,18 +18,42 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        cluster = Cluster.get_or_none("malaria_monthly_routine")
-        periods = MonthPeriod.all_from(MonthPeriod.from_url_str("06-2011"))
+        now = timezone.now()
 
-        for period in periods:
-            logger.info(period)
+        for exp in ExpectedReporting.objects.filter(
+                reporting_period__end_on__gte=now,
+                completion_status=u''):
 
-            for entity in cluster.members():
-                qs = ExpectedReporting.objects.filter(
-                    entity=entity, period=period,
-                    report_class__slug__in=["malaria_monthly_routine",
-                                            "malaria_monthly_routine_aggregated"])
+            logger.info(exp)
 
-                if qs.count() == 2:
-                    logger.info("Removing {}".format(qs[1]))
-                    qs[1].delete()
+            params = {
+                'report_class': exp.report_class,
+                'reporting_role': exp.reporting_role,
+
+                'period': exp.period,
+                'within_period': exp.within_period,
+
+                'entity': exp.entity,
+                'within_entity': exp.within_entity,
+
+                'reporting_period': exp.reporting_period,
+                'extended_reporting_period': exp.extended_reporting_period,
+
+                'amount_expected': exp.amount_expected,
+
+                'completion_status': ExpectedReporting.COMPLETION_MISSING,
+            }
+
+            if not ExpectedReporting.objects.filter(**params).count():
+                # no duplicates on this one.
+                logger.info("... No duplicate")
+                continue
+
+            good = ExpectedReporting.objects.filter(**params).get()
+
+            if not exp.satisfied and not exp.arrived_reports.count():
+                logger.info(". DELETETIN exp: {}".format(exp))
+                exp.delete()
+            else:
+                logger.info("CAN'T DELETE EXP as satisfied: {}".format(exp))
+                logger.info(good)
