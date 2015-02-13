d265d31775ee729d04d021b0ce593c9cf4d6538d
renaud gaudin <rgaudin@gmail.com>
2015-02-13 10:49:54 +0000

    Fixed #74: meta numbers for november

---
+++ snisi_maint/management/commands/fix_74_meta_numbers.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: ai ts=4 sts=4 et sw=4 nu
+
+from __future__ import (unicode_literals, absolute_import,
+                        division, print_function)
+import logging
+
+from django.core.management.base import BaseCommand
+
+from snisi_core.models.Periods import MonthPeriod
+from snisi_core.models.Reporting import SNISIReport
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        logger.info("Issue #74. Wrong counts in Agg meta (reg #61)")
+
+        november = MonthPeriod.from_url_str("11-2014")
+
+        for report in SNISIReport.objects.filter(period=november):
+            report = report.casted()
+            if not hasattr(report, 'update_expected_reportings_number'):
+                continue
+
+            logger.info(".{}".format(report))
+            report.update_expected_reportings_number()
+
+        logger.info("done.")
